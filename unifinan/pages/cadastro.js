fbf75df96bce34a7552da77a2ab74590694ee81e
Luccas Burani <luccas-burani@hotmail.com>
2021-09-23 01:54:56 -0300

    telaCadastro

---
+++ unifinan/pages/cadastro.js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function showKey() {
+//   console.log('Clicado');
+//   var senha = document.getElementById("key");
+//   var senha2 = document.getElementById("confKey");
+//   if (senha.type === "password") {
+//     senha.type = "text";
+//     senha2.type = "text";
+
+//   } else {
+//     senha.type = "password";
+//     senha2.type = "password";
+//   }
+// }
+
+export default function Cadastro() {
+  return (
+    <Form>
+      <body>
+        <div class="topnav">
+          <img src="imagens/logo.png" alt="" />
+          <p>UNIFINAN</p>
+          <a href="#home">Home</a>
+          <a href="cadastro.html">Graficos</a>
+          <a href="cadastro.html">Usuario</a>
+          <a href="#contact">Logout</a>
+        </div>
+        <div class="formCadastro">           
+          <form>
+            <input type="text" id="name" name="name" placeholder="Nome Completo"/>
+            <input type="email" id="email" name="email" placeholder="E-Mail"/>
+            <input type="password" id="key" name="key" placeholder="Sua Senha"/>
+            <input type="password" id="confKey" name="confKey" placeholder="Confirme sua Senha"/>
+            <input type="submit" value="Cadastrar"/>
+            <img src="https://cdn0.iconfinder.com/data/icons/ui-icons-pack/100/ui-icon-pack-14-512.png" id="olho" class="olho" onclick="{showKey}"/>
+            <img src="https://cdn0.iconfinder.com/data/icons/ui-icons-pack/100/ui-icon-pack-14-512.png" id="olho2" class="olho2" onclick="{showKey}"/>
+          </form>
+        </div>
+      </body>
+    </Form>
+  )
+}
