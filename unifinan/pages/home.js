609726bed5c17d972fe9c24911117a218551abb0
andrecrozatti <57077342+andrecrozatti@users.noreply.github.com>
2021-12-07 19:19:53 -0300

    07/12 Andre 19:19

--- unifinan/pages/home.js
+++ unifinan/pages/home.js
@@ -158,8 +158,11 @@ export default function Home() {
                     <div className="col-10"><h5>Sua atividade:</h5></div>
                     <div className="col-2">
                         {/* <a href="./transacoes" className="btn btn-success float-end text-white" title="Novas Transações"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z"/></svg></a> */}
-                        <Link href="/transacoes" className="btn btn-success float-end text-white"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z"/></svg></Link>
-
+                        <Link href="/transacoes">
+                            <a className="btn btn-success float-end text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z"/></svg>
+                            </a>
+                        </Link>
                     </div>
                 </div>
                 <div className="row container_cards">
