cceaeae7e42e6b3ac386d0e2a5addfd6a382fd49
andrecrozatti <57077342+andrecrozatti@users.noreply.github.com>
2021-12-07 18:58:45 -0300

    07/12 Andre

--- unifinan/components/Header/index.js
+++ unifinan/components/Header/index.js
@@ -1,11 +1,11 @@
 import header from "./header.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Link from "next/link"
 
 export default function Header() {
 
-    const data = new Date()   
-    const mes = data.toLocaleString('default', { month: 'numeric' }) 
+    const data = new Date()
+    const mes = data.toLocaleString('default', { month: 'numeric' })
     const ano = data.toLocaleString('default', { year: 'numeric' })
     return (
         <header className={header.mainContainer}>
@@ -13,22 +13,38 @@ export default function Header() {
                 <div className="row">
                     <div className="col-4 col-md-4 col-lg-4 col-xl-4 d-flex">
                         <div className={header.alinha_logo}>
-                            <img src="/logo.png" className="rounded-circle mr-5" style={{height: "50px", width: "50px", marginRight: "10px"}}/>
+                            <img src="/logo.png" className="rounded-circle mr-5" style={{ height: "50px", width: "50px", marginRight: "10px" }} />
                             <span className={header.nomeEmpresa}>Unifinan</span>
                         </div>
                     </div>
                     <div className="col-4 col-md-4 col-lg-4 col-xl-4 text-center">
                         <div className={"row mt-3"}>
-                            <div className="col-4 col-md-4 col-lg-4 col-xl-4"><a className={header.links} href={"./home"}>Visão Geral</a></div>
-                            <div className="col-4 col-md-4 col-lg-4 col-xl-4"><a className={header.links} href={"./relatorios?conta=1&mes="+ mes +"&ano="+ ano +"&pagina=0&itensPorPagina=100"}>Relatórios</a></div>
-                            <div className="col-4 col-md-4 col-lg-4 col-xl-4"><a className={header.links} href={"./categorias"}>Categorias</a></div>
+                            
+                            <div className="col-4 col-md-4 col-lg-4 col-xl-4">
+                                <Link href="/home">
+                                    <a className={header.links}>Visão Geral</a>
+                                </Link>
+                            </div>
+                            <div className="col-4 col-md-4 col-lg-4 col-xl-4">
+                                <Link href="/categorias">
+                                    <a className={header.links}>Categorias</a>
+                                </Link>
+                            </div>
+                            <div className="col-4 col-md-4 col-lg-4 col-xl-4">
+                                <Link href={"./relatorios?conta=1&mes=" + mes + "&ano=" + ano + "&pagina=0&itensPorPagina=100"}>
+                                    <a className={header.links}>Relatórios</a>
+                                </Link>
+                            </div>
+
 
                         </div>
                     </div>
                     <div className="col-4 col-md-4 col-lg-4 col-xl-4 text-end">
                         <div>
                             {/* <img src="/user-icon.png" className={header.headerIcon} /> */}
-                            <a href="/"><img src="/logout.png" className={header.headerIcon} /> </a>
+                            <Link href="/">
+                                <img src="/logout.png" className={header.headerIcon} /> 
+                            </Link>
                         </div>
                     </div>
                 </div>
